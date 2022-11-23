da447a0fd2f853be042391b3194563a977425b44
Aleksandr Minayev <tenshisasha@gmail.com>
2022-11-23 21:20:25 +0300

    some changes

---
+++ ServerAPI/Helpers/ErrorHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ServerAPI.Helpers
+{
+    public class ErrorHelper
+    {
+        public static List<string> AddIdentityErrors(IdentityResult identityResult)
+        {
+            var errors = new List<string>();
+
+            foreach (var e in identityResult.Errors)
+            {
+                errors.Add(e.Description);
+            }
+
+            return errors;
+        }
+
+        public static List<string> AddError(string description)
+        {
+            var errors = new List<string>
+            {
+                description
+            };
+
+            return errors;
+        }
+    }
+}
